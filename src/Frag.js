cf124c2bdd1e6abd36e93e54af97f5ecdfd78ee4
陈文杰 <wutianba@sina.com>
2018-11-05 14:11:10 +0800

    initial

---
+++ src/Frag.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react'
+
+class Frag extends Component {
+  render () {
+    return <Fragment>
+      <p>line 1</p>
+      <p>line 2</p>
+    </Fragment>
+  }
+}
+
+export default Frag
